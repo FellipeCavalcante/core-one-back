64b873b0753eabac2f44248f819231a20410fa92
FellipeCavalcante <azevedoaranha1@gmail.com>
2025-09-15 17:29:58 -0300

    Fixed mappers

--- src/main/java/com/findjobs/back/mapper/SectorMapper.java
+++ src/main/java/com/findjobs/back/mapper/SectorMapper.java
@@ -4,12 +4,14 @@ import com.findjobs.back.domain.Sector;
 import com.findjobs.back.dto.sector.CreateSectorRequestDTO;
 import com.findjobs.back.dto.sector.CreateSectorResponseDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface SectorMapper {
 
     // DTO -> Entity
+    @Mapping(target = "id", ignore = true)
     Sector toSector(CreateSectorRequestDTO dto);
 
     // Entity -> DTO
