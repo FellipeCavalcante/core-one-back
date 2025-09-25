d8766d1ab47eb3100fbf865d84473586d47262a6
FellipeCavalcante <azevedoaranha1@gmail.com>
2025-09-25 18:30:41 -0300

    new helper for verify authenticate user credentials

---
+++ src/main/java/com/coreone/back/util/AuthUtil.java
@@ -0,0 +1,23 @@
+package com.coreone.back.util;
+
+import com.coreone.back.domain.User;
+import com.coreone.back.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AuthUtil {
+
+    private final UserRepository userRepository;
+
+    public User getAuthenticatedUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String email = auth.getName();
+
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not Found!"));
+    }
+}
