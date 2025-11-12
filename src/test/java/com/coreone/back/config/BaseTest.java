535495a6157acb30039377c05894e843014c8f40
FellipeCavalcante <azevedoaranha1@gmail.com>
2025-11-12 15:18:07 -0300

    disable emailService test

--- src/test/java/com/coreone/back/config/BaseTest.java
+++ src/test/java/com/coreone/back/config/BaseTest.java
@@ -4,10 +4,12 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
+@Import(TestConfig.class)
 public abstract class BaseTest {
 
     @LocalServerPort
